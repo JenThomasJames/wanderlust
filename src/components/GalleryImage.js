e037b3085c7ea994f9d92ae707ba04a0fc99e6f2
JenThomasJames <jenthomasjames123@gmail.com>
2022-10-07 12:56:52 +0530

    Added alt text to GalleryImage

--- src/components/GalleryImage.js
+++ src/components/GalleryImage.js
@@ -1,5 +1,5 @@
 import styles from "./GalleryImage.module.css";
 const GalleryImage = (props) => {
-  return <img className={styles.image} src={props.source} />;
+  return <img className={styles.image} src={props.source} alt={"country"} />;
 };
 export default GalleryImage;
